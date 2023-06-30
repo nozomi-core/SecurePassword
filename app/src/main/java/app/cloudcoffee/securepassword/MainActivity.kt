19db238ad1c24f7d55bc23eb8f5027052964448a
PhoenixShell <app.phoenixshell@gmail.com>
2023-06-30 15:32:18 +1000

    Add koin

--- app/src/main/java/app/cloudcoffee/securepassword/MainActivity.kt
+++ app/src/main/java/app/cloudcoffee/securepassword/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import app.cloudcoffee.securepassword.ui.theme.SecurePasswordTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
